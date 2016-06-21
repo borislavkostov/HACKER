30e4152e3e4ff65b7ad73c707ae5c38b07e96d4a
borislavkostov <borislavkostov97@gmail.com>
2016-06-22 00:16:31 +0300

    chapter 3

---
+++ Python Projects from book/3_chapter/3_01.py
@@ -0,0 +1,6 @@
+a=input('Vuvedete chishlo: ')
+a=int(a)
+if a%2==0:
+      print('even')
+else:
+      print('odd')
---
+++ Python Projects from book/3_chapter/3_03.py
@@ -0,0 +1,4 @@
+a=input('Vuvedete chislo: ')
+a=int(a)
+stotici=a%100
+print(stotici%10==7)
