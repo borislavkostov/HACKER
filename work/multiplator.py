975587a8bb40cb5d3dce46cbfa66cc40764e4a7f
borislavkostov <borislavkostov97@gmail.com>
2016-06-27 11:38:13 +0300

    if added

--- work/multiplator.py
+++ work/multiplator.py
@@ -1,10 +1,13 @@
 import math
 n=int(input("Enter n = "))
 numbers =[1]
-for x in range(1,3200000):
-      result=x*x
-      numbers.append(result)
+if n<3200000:
+      for x in range(1,3200000):
+            result=x*x
+            numbers.append(result)
 
-str1 = ''.join(str(e) for e in numbers)
-print(str1[n])
+      str1 = ''.join(str(e) for e in numbers)
+      print(str1[n])
+else:
+      print("Out of scope !")
 
