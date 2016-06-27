0b09004153a1bdb53fab273e3062c881d2b65711
borislavkostov <borislavkostov97@gmail.com>
2016-06-27 11:07:30 +0300

    multiplator

---
+++ work/multiplator.py
@@ -0,0 +1,10 @@
+import math
+n=int(input("Enter n = "))
+numbers =[1]
+for x in range(1,32000):
+      result=x*x
+      numbers.append(result)
+
+str1 = ''.join(str(e) for e in numbers)
+print(str1[n])
+
