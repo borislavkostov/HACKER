3a27589e36e66ae1340b1ea0e3b3de7ee019af67
borislavkostov <borislavkostov97@gmail.com>
2016-06-27 09:32:43 +0300

    Chapter_4

---
+++ Python Projects from book/4_chapter/4_01.py
@@ -0,0 +1,5 @@
+a=int(input('Enter a ='))
+b=int(input('Enter b ='))
+c=int(input('Enter c ='))
+result=a+b+c
+print(result)
---
+++ Python Projects from book/4_chapter/4_02.py
@@ -0,0 +1,6 @@
+import math
+r=float(input('Enter r = '))
+p=2*math.pi*r
+print('P =',p)
+s=math.pi*math.pow(r,2)
+print('S = ',s)
